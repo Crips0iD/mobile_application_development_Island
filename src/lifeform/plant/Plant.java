d2f889b1b0fd6fb357260703cbcaab6d81ee6230
Crips0iD <baevbaev1882@mail.ru>
2025-03-28 15:19:03 +0300

    Initial commit

---
+++ src/lifeform/plant/Plant.java
@@ -0,0 +1,12 @@
+package lifeform.plant;
+
+import lifeform.LifeForm;
+
+public class Plant extends LifeForm {
+    /**
+     * Конструктор класса Plant
+     */
+    public Plant() {
+        super(1, 200, "Plant");
+    }
+}
